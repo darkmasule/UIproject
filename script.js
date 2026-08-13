155047964c27967e0b3d139e7b0d1d5bd5a90368
Grimm <lastexcaliber@gmail.com>
2026-08-13 16:36:32 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const inputName = document.getElementById('username')
 const inputPassword = document.getElementById('password')
-const register = document.getElementById('register')
 const login = document.getElementById('login')
-const listed = document.getElementById('listed')
+const signup = document.getElementById('signup')
+
+const rgUsername = document.getElementById('rgusername')
+const rgEmail = document.getElementById('rgemail')
+const rgPassword = document.getElementById('rgpassword')
+const register = document.getElementById('register')
+
+const registerpanel = document.getElementById('registerpanel')
+const rpclose = document.getElementById('rpclose')
 
 let accountList = JSON.parse(localStorage.getItem('accountList'));
 
@@ -12,12 +19,20 @@ if (accountList === null) {
 
 register.addEventListener('click', () => {
     
-    if (accountList.find(account => account.username === inputName.value)) {
+    if (accountList.find(account => account.username === rgUsername.value)) {
         alert('This username has been taken');
-    } else if (inputName.value === '') {
-        alert('Empty username');
-    } else if (inputPassword.value === '' ) {
-        alert('Empty password')        
+    } else if (rgUsername.value === '') {
+        alert('Invalid username');
+    } else if (rgUsername.value.includes(' ')) {
+        alert('Invalid username');
+    } else if (rgEmail.value.includes(' ')) {
+        alert('Invalid email');
+    } else if (rgEmail.value === '') {
+        alert('Invalid email');
+    } else if (!rgEmail.value.endsWith('@gmail.com') && !rgEmail.value.endsWith('@outlook.com') && !rgEmail.value.endsWith('@yahoo.com') && !rgEmail.value.endsWith('@icloud.com') && !rgEmail.value.endsWith('@hotmail.com')) {
+        alert('Invalid email');
+    } else if (rgPassword.value === '' || rgPassword.value.includes(' ')) {
+        alert('Invalid password')        
     } else {
         const account = {
         username: inputName.value,
@@ -32,7 +47,7 @@ register.addEventListener('click', () => {
     localStorage.setItem('accountList', JSON.stringify(accountList));
 
     }
-    
+
     console.log(accountList);
 })
 
@@ -44,8 +59,10 @@ login.addEventListener('click', () => {
     }
 })
 
-let usernamelist = accountList[0];
-console.log(usernamelist);
-accountList.forEach(account => {
-listed.innerText += account.username + '\n';
-});
+signup.addEventListener('click', () => {
+    registerpanel.className = 'registerpanel2'
+})
+
+rpclose.addEventListener('click', () => {
+    registerpanel.className = 'registerpanel'
+})
